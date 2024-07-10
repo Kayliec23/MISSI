b77f7f704bfc01cf4a5d6bf49811999a1b860b84
Kaylie Cohanim <38339219+Kayliec23@users.noreply.github.com>
2024-07-10 09:17:48 -0700

    Update PVtendency.m

--- PVtendency.m
+++ PVtendency.m
@@ -4,9 +4,8 @@
 %%% Um_Diss - U momentum tendency from Dissipation (Explicit part)
 %%% Um_Impl - U momentum tendency from Dissipation (Implicit part)
 %%% Um_Advec - U momentum tendency from Advection terms
-%%% Um_Cori - U momentum tendency from Coriolis term
-%%% Um_dPhiX - U momentum tendency from Pressure/Potential grad
-%%% USidDrag - U momentum tendency from Side Drag
+%%% Um_Cori - U momentum tendency from Coriolis term (this term is part of Um_Advec in current model setting) 
+%%% Um_dPhiX - U momentum tendency from Pressure/Potential grad (in 2D this term is zero)
 %%% ADVx_TH - Zonal Advective Flux of Pot.Temperature
 %%% DFxE_TH - Zonal Diffusive Flux of Pot.Temperature
 %%% ADVx_SLT - Zonal Advective Flux of Salinity
@@ -15,104 +14,123 @@
 %%% TOTTTEND - Tendency of Potential Temperature
 %%% TOTSTEND - Tendency of Salinity
 
-%%% define b
-% calculate rho
-rho = NaN(Nx,Ny,Nr,Nt);
-for n = 1:Nt
-  SS = squeeze(S(1,:,:,n));
-  TT = squeeze(T(1,:,:,n));
-  rho(Nx,:,:,n) = densjmd95(SS,TT,0);
-end
+% Constants
+Nx = length(delX); 
+Ny = length(delY);
+Nr = length(delR);
+Nt = size(T,4);
+dy = diff(yy,1);
+dx = diff(xx,1);
+dz = diff(zz,1);
+Omega = 2*pi*366/365/86400;
+lat0 = -80; %%% Latitude at southern boundary
+f0 = 2*Omega*sind(lat0); %%% Coriolis parameter
+m1km = 1000;
+g = 9.81;
+rho0 = 1027;
+t1min = 50;
+t1hour = 60*t1min;
+t1day = 24*t1hour;
+[YY,ZZ] = meshgrid(yy,zz);
 
-b = -g/rho0*rho;
+addpath /Users/Kayliec23/Desktop/analysis
 
-%%% define bdot
-% calculate rho_dot from TOTSTEND and TOTTTEND
-rho_dot = NaN(Nx,Ny,Nr,Nt);
-for n = 1:Nt
-  SS = squeeze(TOTSEND(1,:,:,n));
-  TT = squeeze(TOTTEND(1,:,:,n));
-  rho_dot(Nx,:,:,n) = densjmd95(SS,TT,0);
-end
-bdot = -g/rho0*rho_dot;
+%%
+%%%%%%%%%%%%%%%%%%%%%%%%%% define b and u and their components %%%%%%%%%%%%%%%%%%%%%%%%
+ 
+%%% define b and its components
+b = buoyancy(S,T);
+bdot = (b(:,:,:,2:Nt)-b(:,:,:,1:Nt-1)) ./ t1hour; 
 
-% calculate rho_diff
-rho_diff = NaN(Nx,Ny,Nr,Nt);
-for n = 1:Nt
-  SS = squeeze(DFxE_SLT(1,:,:,n));
-  TT = squeeze(DFxE_TH(1,:,:,n));
-  rho_diff(Nx,:,:,n) = densjmd95(SS,TT,0);
-end
-bdot_diff = -g/rho0*rho_diff;
 
-% calculate rho_adv
-rho_adv = NaN(Nx,Ny,Nr,Nt);
-for n = 1:Nt
-  SS = squeeze(ADVx_SLT(1,:,:,n));
-  TT = squeeze(ADVx_TH(1,:,:,n));
-  rho_adv(Nx,:,:,n) = densjmd95(SS,TT,0);
-end
-bdot_adv = -g/rho0*rho_adv;
+% % calculate rho_diff
+% bDiff = buoyancy(SdiffX,THdiffX);
+% 
+% % calculate rho_adv
+% bAdv = buoyancy(SadvX,THadvX);
+% 
+% % calculate rho_adv
+% bdot = buoyancy(TOTSTEND,TOTTTEND);
 
-%%% define u
+%%% define u and its components
 u = U;
 
 %%% define udot
-udot = TOTUTEND;
-udot_diss = Um_diss;
-udot_adv = Um_Advec;
-udot_cori = Um_cori;
-udot_p = Um_dPhiX;
+udot = TOTUTEND/86400;
+%Udiss = Um_Diss + Um_ImplD;
 
+
+%%%%%%%%%%%%%%%%%%%%%%%%%% calculate qdot mean terms %%%%%%%%%%%%%%%%%%%%%%%%
+ 
 %%% term1 
-A = mean(f0*bdot,2,'omitnan');    % mean in y
+A = nanmean(f0*bdot,2,'omitnan');    % mean in y
 term1 = (diff(A,1,3))./dz;    % diff in z
-term1= interp_center_y(term1);    % interpolate and center in z direction
+term1= interp_center_y(term1,Ny);    % interpolate and center in z direction --> mean in z
 
 %%% term2 
-A = mean(diff(udot,1,2),3); % diff in y --> mean in z
-A = interp_center_y(A);    % interpolate and center in z direction
-B = mean(diff(b,1,3),2); % diff in z --> mean in y
-B = interp_center_z(B);    % interpolate and center in z direction
+A = diff(udot,1,2); % diff in y 
+A = nanmean(interp_center_y(A,Ny),3);    % interpolate and center in y direction --> mean in z
+B = diff(b,1,3); % diff in z 
+B = nanmean(interp_center_z(B,Nr),2);    % interpolate and center in z direction --> mean in y
 term2 = -(A.*B)./dy./dz;
 
 %%% term3
-A = mean(diff(u,1,2),3); % diff in y --> mean in z
-A = interp_center_y(A);    % interpolate and center in z direction
-B = mean(diff(bdot,1,3),2);    % diff in z --> mean in y
-B = interp_center_z(B);    % interpolate and center in z direction
+A = diff(u,1,2); % diff in y 
+A = nanmean(interp_center_y(A,Ny),3);    % interpolate and center in z direction --> mean in z
+B = diff(bdot,1,3);    % diff in z 
+B = nanmean(interp_center_z(B,Nr),2);    % interpolate and center in z direction --> mean in y
 term3 = -(A.*B)./dy./dz;
 
 %%% term4 
-A = mean(diff(udot,1,3),2); % diff in z --> mean in y
-A = interp_center_z(A);    % interpolate and center in z direction
-B = mean(diff(b,1,2),3);   % diff in y --> mean in z
-B = interp_center_y(B);    % interpolate and center in z direction
+A = diff(udot,1,3); % diff in z 
+A = nanmean(interp_center_z(A,Nr),2);    % interpolate and center in z direction --> mean in y
+B = diff(b,1,2);   % diff in y 
+B = nanmean(interp_center_y(B,Ny),3);    % interpolate and center in z direction --> mean in z
 term4 = (A.*B)./dy.dz;
 
 %%% term5 
-A = mean(diff(u,1,3),2);    % diff in z --> mean in y
-A = interp_center_z(A);    % interpolate and center in z direction
-B = mean(diff(bdot,1,2),3);    % diff in y --> mean in z
-B = interp_center_y(B);    % interpolate and center in z direction
+A = diff(u,1,3);    % diff in z
+A = nanmean(interp_center_z(A,Nr),2);    % interpolate and center in z direction --> mean in y
+B = diff(bdot,1,2);    % diff in y 
+B = nanmean(interp_center_y(B,Ny),3);    % interpolate and center in z direction --> mean in z
 term5 = (A.*B)./dy./dz;
 
 %%% qdot
 qdot = term1 + term2 + term3 + term4 + term5; 
 
+%%%%%%%%%%%%%%%%%%%%%%%%%% calculate qdot perturbations terms %%%%%%%%%%%%%%%%%%%%%%%%
+qmean = 
 
-function BB = interp_center_z(AA)
+
+
+
+
+
+
+
+
+function BB = interp_center_z(AA,Nr)
   AA(:,:,Nr,:) = 2*AA(:,:,Nr-1,:) - AA(:,:,Nr-2,:);    % interpolate in z for boundary point
   AA = ( AA(:,1:Nr-1,:,:) + AA(:,2:Nr,:,:) ) / 2;    % recenter
   BB = AA;
 end
 
-function BB = interp_center_y(AA)
+function BB = interp_center_y(AA,Ny)
   AA(:,Ny,:,:) = 2*AA(:,Ny-1,:,:)-AA(:,Ny-2,:,:);    % interpolate in y for boundary point
   AA = ( AA(:,1:Ny-1,:,:) + AA(:,2:Ny,:,:) ) / 2;    % recenter
   BB = AA;
 end
 
+function b = buoyancy(SS,TT)
+    S = squeeze(SS(1,:,:,1));
+    T = squeeze(TT(1,:,:,1));
+    g = 9.81;
+    dPT = 1e-3;
+    dSP = 1e-3;
+    alpha = - (densjmd95(S,T+0.5*dPT,0) - densjmd95(S,T-0.5*dPT,0))./densjmd95(S,T,0)/dPT;
+    beta = (densjmd95(S+0.5*dSP,T,0) - densjmd95(S-0.5*dSP,T,0))./densjmd95(S,T,0)/dSP;
+    b = g.*(alpha.*T-beta.*S);
+end
 
 
 
